7c7425e0bb5ae4d7e9c85bddb419c61d3ed0bf64
leandrodrey <leandrodrey@gmail.com>
2021-08-27 22:14:46 -0300

    This Service will return the location for the message emitter

---
+++ app/Services/GetLocationService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use App\Domain\Satellite;
+use App\Domain\RequestedSatellite;
+use App\Domain\SatelliteFactory;
+use App\Util\Trilateration\Intersection;
+use App\Util\Trilateration\Point;
+
+class GetLocationService
+{
+
+    /** @var SatelliteFactory */
+    private SatelliteFactory $factory;
+
+    public function __construct(SatelliteFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * @param array $requestedSatellites
+     * @return Point
+     */
+    function getLocation(array $requestedSatellites): Point
+    {
+        $satellites = $this->factory->buildSatellites($requestedSatellites);
+        $trilateration = new Intersection($satellites[0]->createNewSphere(), $satellites[1]->createNewSphere(), $satellites[2]->createNewSphere());
+        return $trilateration->position();
+    }
+
+}
