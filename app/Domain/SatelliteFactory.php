93226200b0edc349c483e667abda254cb47a93d0
leandrodrey <leandrodrey@gmail.com>
2021-08-27 22:13:23 -0300

    This factory will create new satellites and an array of satellites

---
+++ app/Domain/SatelliteFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Domain;
+
+class SatelliteFactory
+{
+
+    /** @var array|float[][] */
+    static array $satellitePosition = [
+        "kenobi" => [60.1695,24.9354],
+        "skywalker" => [58.3806, 26.7251],
+        "sato" => [58.3859, 24.4971]
+    ];
+
+    /**
+     * @param array $requestedSatellites
+     * @return array
+     */
+    public function buildSatellites(array $requestedSatellites):array {
+        $satellites = [];
+        foreach ($requestedSatellites as $satellite) {
+            $satellites [] = $this->buildSatellite($satellite["name"], $satellite["distance"], $satellite["message"]);
+        }
+        return $satellites;
+    }
+
+    /**
+     * @param string $satelliteName
+     * @param float $satelliteDistance
+     * @param array $satelliteMessage
+     * @return Satellite
+     */
+    public function buildSatellite(string $satelliteName, float $satelliteDistance, array $satelliteMessage): Satellite
+    {
+        return new Satellite($satelliteName, $satelliteDistance, $satelliteMessage, self::$satellitePosition[$satelliteName]);
+    }
+
+}
