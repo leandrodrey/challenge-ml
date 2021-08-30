42880bcca3dffb39bcb8d8df580c20c6f5bcf934
leandrodrey <leandrodrey@gmail.com>
2021-08-30 00:07:37 -0300

    Table satellites

---
+++ database/migrations/2014_10_12_000000_create_satellites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSatelliteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('satellites', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('distance');
+            $table->string('message');
+            $table->string('position');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('satellites');
+    }
+}
